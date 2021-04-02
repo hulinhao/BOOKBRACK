02b4f85fa3ef56611afd54a602614df1651f9dd9
hulinhao <hulinhao@beadwallet.com>
2021-04-02 14:39:05 +0800

    20210402

---
+++ pages/shop/index.js
@@ -0,0 +1,17 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imageurl: ['https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01570455437bb90000019ae9743383.jpg%402o.jpg&refer=http%3A%2F%2Fimg.zcool.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1619855406&t=d3d2eaae757dd3535d6b63b9d2ee8125',
+          'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3834515775,2104781929&fm=26&gp=0.jpg',
+          'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=454208031,2427110626&fm=26&gp=0.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2500,
+    duration: 1000
+  },
+
+})
